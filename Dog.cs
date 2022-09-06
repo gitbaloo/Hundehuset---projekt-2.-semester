998024a9219c7fec2cec1528d5a12f684892026c
gitbaloo <102722373+gitbaloo@users.noreply.github.com>
2022-09-06 09:17:17 +0200

    Add project files.

---
+++ Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hundehuset
+{
+    class Dog
+    {
+        public string pedigreeNumber { get; set; }
+        public string name { get; set; }
+        public DateTime birthDate { get; set; }
+        public char sex { get; set; }
+        public int chipNumber { get; set; }
+        public double inbreedingCoefficient { get; set; }
+        public char hdStatus { get; set; }
+        public int hdIndex { get; set; }
+        public int spondylosisStatus { get; set; }
+        public int heartStatus { get; set; }
+        public string colour { get; set; }
+        public bool isAlive { get; set; }
+        public string momPedigreeNumber { get; set; }
+        public string dadPedigreeNumber { get; set; }
+        public string owner { get; set; }
+        public string breeder { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hundehuset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who let the dogs out?");
+            Console.ReadKey();
+            Console.WriteLine("Woof, woof, woof!");
+        }
+    }
+}
