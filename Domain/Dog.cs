189ba3c14a451e123f442f5e428481b5a1dc9526
TriDaK <trinekremmling@gmail.com>
2022-09-07 11:31:57 +0200

    changed Dog attribute chipNumber from int to string

--- Domain/Dog.cs
+++ Domain/Dog.cs
@@ -12,7 +12,7 @@ namespace Hundehuset.Domain
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public char Sex { get; set; }
-        public int ChipNumber { get; set; }
+        public string ChipNumber { get; set; }
         public double InbreedingCoefficient { get; set; }
         public char HdStatus { get; set; }
         public int HdIndex { get; set; }
