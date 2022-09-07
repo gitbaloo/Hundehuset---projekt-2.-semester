a6d0a960fe8cccb006df27b8c0b8243beb4dca11
BIIIIIIIIIIIIIRD <jan.7100@hotmail.com>
2022-09-07 18:32:04 +0200

    ShowProfileDogView()

--- UI/DogView.cs
+++ UI/DogView.cs
@@ -314,5 +314,36 @@ namespace Hundehuset.UI
                 }
             }
         }
+
+        public void ShowProfileDogView(Dog dog)
+        {
+            /*string Status;
+            if (dog.IsAlive)
+                Status = "Alive";
+            else
+                Status = "Deceased";
+            Console.WriteLine("Status: {0}", Status);*/
+
+            Console.Clear();            
+            Console.WriteLine("Showing profile for {0} ({1})", dog.Name, dog.PedigreeNumber);
+            Console.WriteLine();
+            Console.WriteLine("Id: {0}", dog.Id);
+            Console.WriteLine("Pedigree Number: {0}", dog.PedigreeNumber);
+            Console.WriteLine("Name: {0}", dog.Name);
+            Console.WriteLine("Birth Date: {0}", dog.BirthDate);
+            Console.WriteLine("Sex: {0}", dog.Sex);
+            Console.WriteLine("Chip Number: {0}", dog.ChipNumber);
+            Console.WriteLine("Inbreeding Coefficient: {0}", dog.InbreedingCoefficient);
+            Console.WriteLine("HD-Status: {0}", dog.HdStatus);
+            Console.WriteLine("HD-Index: {0}", dog.HdIndex);
+            Console.WriteLine("Spondylosis Status: {0}", dog.SpondylosisStatus);
+            Console.WriteLine("Heart Status: {0}", dog.HeartStatus);
+            Console.WriteLine("Color: {0}", dog.Color);
+            Console.WriteLine("Status: {0}", dog.IsAlive);
+            Console.WriteLine("Mom's Pedigree Number: {0}", dog.MomPedigreeNumber);
+            Console.WriteLine("Dad's Pedigree Number: {0}", dog.DadPedigreeNumber);
+            Console.WriteLine("Owner: {0}", dog.Owner);
+            Console.WriteLine("Breeder: {0}", dog.Breeder);
+        }
     }
 }
