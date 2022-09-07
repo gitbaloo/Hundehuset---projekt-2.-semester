2e8a028812b1b0c5e49af08ccb62c9ae4f22cec7
TriDaK <trinekremmling@gmail.com>
2022-09-07 21:01:02 +0200

    Updates to NavigationView. Added loop and the posibility to close the program

--- UI/NavigationView.cs
+++ UI/NavigationView.cs
@@ -12,29 +12,43 @@ namespace Hundehuset.UI
 
         public void menu()
         {
+            //Only when the system starts up
             Console.WriteLine("Who let the dogs out?");
             Console.ReadKey();
             Console.WriteLine("Woof, woof, woof!");
-            Console.WriteLine();
+            Console.ReadKey();
 
-            Console.WriteLine("Select menu");
-            Console.WriteLine("1: Register new dog");
-            Console.WriteLine("2: Search for a dog");
-            var choice = Console.ReadLine();
+            // Loop of menu, that loops until the close program is selected
+            bool runProgram = true;
 
-            switch (choice)
+            while (runProgram == true)
             {
-                case "1":
-                    dogview.RegisterDog();
-                    break;
+                Console.Clear(); // the menu gets presented on a cleared console
 
-                case "2":
-                    dogview.SearchDogView();
-                    break;
+                // Menu
+                Console.WriteLine("Selection menu:");
+                Console.WriteLine("\t1: Register new dog.");
+                Console.WriteLine("\t2: Search for a dog.");
+                Console.WriteLine("\tX: Close program.");
 
-                default:
-                    Console.WriteLine("Invalid choice. Please try again");
-                    break;
+                // user selection
+                var choice = Console.ReadLine().ToUpper();
+
+                switch (choice)
+                {
+                    case "1":
+                        dogview.RegisterDog();
+                        break;
+                    case "2":
+                        dogview.SearchDogView();
+                        break;
+                    case "X":
+                        runProgram = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again");
+                        break;
+                }
             }
         }
     }
